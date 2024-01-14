7a6f08dddcb89ae4b6f463b79dac73747066ed79
matsunaganaoya001 <148695820+matsunaganaoya001@users.noreply.github.com>
2024-01-15 00:59:35 +0900

    Add files via upload

--- samuraitravel/src/main/java/com/example/samuraitravel/controller/HomeController.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/HomeController.java
@@ -1,13 +1,12 @@
 package com.example.samuraitravel.controller;
 
-import org.springframework.stereotype.Controller;
+ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+ @Controller
 public class HomeController {
-	@GetMapping("/")
-	public String index(){
-		return "index";
-	}
-
+     @GetMapping("/")
+     public String index() {
+         return "index";
+     }   
 }
